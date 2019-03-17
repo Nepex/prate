e47b17e4e908119a5f830025ec0f14e2ed2efa07
nepex <mattb103190@gmail.com>
2019-03-17 17:48:13 -0500

    modal adjustments

--- src/app/shared/form-validation-msgs/form-validation-msgs.component.ts
+++ src/app/shared/form-validation-msgs/form-validation-msgs.component.ts
@@ -5,24 +5,13 @@ import { FormControl } from '@angular/forms';
     selector: 'prt-form-validation-msgs',
     template:
     `<span *ngIf=" ( control.touched || submitted) && control.errors">
-        <span class="form-control-feedback float-right" *ngIf="control.hasError('required')">
-            <i class="fa fa-warning"></i>&nbsp;Required
-        </span>
-        <span class="form-control-feedback float-right" *ngIf="control.hasError('minlength')">
-            <i class="fa fa-warning"></i>&nbsp;At least {{ control.errors.minlength.requiredLength }} characters required.
-        </span>
-        <span class="form-control-feedback float-right" *ngIf="control.hasError('maxlength')">
-            <i class="fa fa-warning"></i>&nbsp;Cannot exceed {{ control.errors.maxlength.requiredLength }} characters.
-        </span>
-        <span class="form-control-feedback float-right" *ngIf="control.hasError('pattern')">
-            <i class="fa fa-warning"></i>&nbsp;Does not meet required format.<span *ngIf="example"> Example: {{ example }}</span>
-        </span>
-        <span class="form-control-feedback float-right" *ngIf="control.hasError('match')">
-            <i class="fa fa-warning"></i>&nbsp;Passwords do not match.
+        <span class="form-control-feedback" *ngIf="control.hasError('required') || control.hasError('minlength') || control.hasError('maxlength') || control.hasError('pattern') ||
+        control.hasError('match')">
+            <i class="fas fa-times"></i>
         </span>
     </span>
     <span *ngIf="(control.touched || submitted) && !control.errors">
-        <span class="checkmark float-right">&#10003;</span>
+        <span class="checkmark">&#10003;</span>
     </span>`,
     styleUrls: ['./form-validation-msgs.component.css']
 })
