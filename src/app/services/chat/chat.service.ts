5d215ce516a7b504c03fba54463553cb44411d63
nepex <thechump@hotmail.com>
2020-05-05 11:34:12 -0500

    new fonts

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -25,13 +25,13 @@ export class ChatService implements OnDestroy {
 
     /** Emits object to socket of user currently looking for match, looks for a match every second if nothing is returned */
     intiateMatching(user: User): void {
-        this.socket = io('http://localhost:3000', {
-            path: '/socket.io'
-        });
-
-        // this.socket = io('https://prate.club', {
-        //     path: '/api/socket.io'
+        // this.socket = io('http://localhost:3000', {
+        //     path: '/socket.io'
         // });
+
+        this.socket = io('https://prate.club', {
+            path: '/api/socket.io'
+        });
 
         // this.socket = io.connect(environment.apiBaseUrl);
 
